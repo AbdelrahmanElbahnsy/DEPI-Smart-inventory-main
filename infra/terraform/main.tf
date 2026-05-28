412dc2a48f4246dc56531766b06f19ecbff71343
Abdelrahman Elbahnsy <abdelrahmanelbahnsy3@gmail.com>
2026-05-28 23:16:52 +0300

    feat: update ansible playbook with dynamic env vars

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -113,6 +113,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+      name                       = "Jenkins"
+      priority                   = 1006
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8080"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
 }
 
 # ✅ التعديل هنا: الـ IP أصبح Static (ثابت) مع SKU Standard
